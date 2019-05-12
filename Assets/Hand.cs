27bf9d70994e7f69713ea0b8580d7c3bb2303cf0
Michał Słowikowski <slovik421@gmail.com>
2019-05-12 17:23:49 +0200

    Added basic hand controls

---
+++ Assets/Hand.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public bool isPlayer = false;
+    public float maxThrowForce = 5f;
+    bool isGrabbing = false;
+    bool isHolding = false;
+    Collider2D handCollider;
+    Collider2D fistCollider;
+    Rigidbody2D holdingObject;
+    Rigidbody2D handBody;
+    Joint2D joint;
+    void Start()
+    {
+        fistCollider = GetComponent<CircleCollider2D>();
+        handBody = GetComponent<Rigidbody2D>();
+        joint = GetComponent<Joint2D>();
+        fistCollider.enabled = false;
+    }
+
+    void FixedUpdate()
+    {
+        if(isPlayer)
+        {
+           
+           handBody.MovePosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+           isGrabbing = Input.GetMouseButton(0);
+
+           if(isGrabbing && !isHolding)
+           {
+               isHolding = true;
+               handCollider = Physics2D.OverlapPoint(handBody.position);
+               if(handCollider!=null)
+               {
+                    holdingObject = handCollider.GetComponent<Rigidbody2D>();
+                    joint.connectedBody = holdingObject;
+               }
+               else
+               {
+                   fistCollider.enabled = true;
+                   Debug.Log($"Now I'm fist!");
+               }
+           }
+           else if(!isGrabbing)
+           {
+               Debug.Log("Now I'm free!");
+               isHolding = isGrabbing;
+               fistCollider.enabled = false;
+               //holdingObject.velocity = Vector3.Normalize(holdingObject.velocity)*maxThrowForce*Time.deltaTime;
+               holdingObject = null;
+               joint.connectedBody = holdingObject;
+           }
+
+           if(isHolding)
+           {
+
+           }
+        }
+    }
+}
---
+++ Assets/Scripts/Arm.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour
+{
+    public Transform Target;
+    public float startWidth = 1f;
+    public float endWidth = 1f;
+    public float downForce = 1f;
+    private LineRenderer line;
+
+    float lineStep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.enabled = true;
+        SetWidth();
+        lineStep = 1f/(line.positionCount - 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetPosition();
+    }
+
+    void SetPosition()
+    {
+        Vector3  middle = Vector3.Lerp(
+            this.transform.position,
+            Target.position,
+            0.5f)+(Vector3.down*downForce);
+
+        for(int i = 0; i < line.positionCount; i++)
+        {
+            float currentPoint = lineStep * i;
+            Vector3 pointPosition = Vector3.Lerp(
+                Vector3.Lerp(this.transform.position,middle,currentPoint),
+                Vector3.Lerp(middle,Target.position,currentPoint),
+                currentPoint);
+            line.SetPosition(i,pointPosition);
+        }
+    }
+    void SetWidth()
+    {
+        line.startWidth = startWidth;
+        line.endWidth = endWidth;
+    }
+}
